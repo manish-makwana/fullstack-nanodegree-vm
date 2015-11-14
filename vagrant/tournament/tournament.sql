c47fa3b666a8a2337c11f4d820500da691668a2a
Manish Makwana <manish.makwana@live.com>
2015-11-14 19:29:47 +1300

    Built and tested tables, with primary keys and foreign keys defined. Appears good.

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,27 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE DATABASE tournament;
+\c tournament
+CREATE TABLE players(id SERIAL PRIMARY KEY,
+										 NAME TEXT);
 
+CREATE TABLE tournaments(id SERIAL PRIMARY KEY,
+												 date_held DATE,
+												 location TEXT);
+
+CREATE TABLE matches(id SERIAL PRIMARY KEY,
+										 winner INTEGER REFERENCES players(id),
+										 loser INTEGER REFERENCES players(id));
+
+CREATE TABLE match_ownership(id SERIAL PRIMARY KEY,
+														 tournament_id INTEGER REFERENCES 
+															 tournaments(id),
+														 match_id INTEGER REFERENCES matches(id));
+
+CREATE TABLE standings(player_id INTEGER REFERENCES players(id),
+											 tournament_id INTEGER REFERENCES tournaments(id),
+											 original_rank INTEGER,
+											 had_bye BOOLEAN,
+											 standing REAL,
+											 PRIMARY KEY(player_id, tournament_id));

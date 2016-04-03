5532c1717d663b04ca214dc5952df8fcaa15f88a
Manish Makwana <manish@Manishs-MBP.lan>
2016-04-03 14:43:52 +1200

    Add hola webpage.

--- vagrant/menus/webserver.py
+++ vagrant/menus/webserver.py
@@ -14,8 +14,25 @@ class webserverHandler(BaseHTTPRequestHandler):
                 print output
                 return
 
+            if self.path.endswith("/hola"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>&#161Hola <a href='/hello'>back to Hello</a></body></html>"
+                self.wfile.write(output)
+                print output
+                return
+
         except IOError:
             self.send_error(404, "File not found %s" % self.path)
+
+    def do_POST(self):
+        try:
+
+        except IOError:
+
 
 def main():
     try:
